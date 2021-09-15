3719b478ab72a1449c01e812ecc40c188b5b5667
Raz Luvaton <rluvaton@gmail.com>
2021-09-15 05:30:59 +0300

    fix(cookies-injector): preserve animation when closing the cookie notice
    Fix #25

--- src/common/components/CookieInjector/index.tsx
+++ src/common/components/CookieInjector/index.tsx
@@ -1,18 +1,23 @@
 import useSingleCookie from '../../hooks/useSingleCookie';
 import useScript, { ScriptLoadingState } from '../../hooks/useScript';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as CookiesLoader from './cookies-loader';
 
 import './index.css';
 
 // @ts-ignore There is no types for this library ¯\_(ツ)_/¯ (https://github.com/xavierbriole/react-cookienotice/issues/71)
 import CookieNotice from 'react-cookienotice'
+import usePrevious from '../../hooks/usePrevious';
 
 const CookieInjector = () => {
   const allowCookies = useSingleCookie({
     cookieName: 'allow-cookies',
     parser: value => value === 'true',
   });
+  const prevAllowCookies = usePrevious(allowCookies);
+
+  // If when first opened the page the cookies was already allowed
+  const wasCookiesAllowedOnFirstRender = useRef(prevAllowCookies === undefined && allowCookies);
 
   const {
     scriptLoadingState: googleAnalyticsScriptLoadedState,
@@ -44,10 +49,19 @@ const CookieInjector = () => {
     CookiesLoader.load();
   }, [setShouldLoadCookies, allowCookies, googleAnalyticsScriptLoadedState]);
 
-  // We're not using the `onAcceptButtonClick` function because it's good only for the first time.
-  // We need to check if the `allow-cookies` exist after the user pressed accept and reloaded the page (for example)
-  // And the function only called when clicking accept...
-  return <CookieNotice/>;
+  return (
+    // Hack for making the `CookieNotice` to not taking any space (Fix #25)
+    <div
+      className={wasCookiesAllowedOnFirstRender.current ? 'on-start-cookies-allowed' : 'on-start-cookies-not-allowed'}>
+
+      {/*
+       We're not using the `onAcceptButtonClick` function because it's good only for the first time.
+       We need to check if the `allow-cookies` exist after the user pressed accept and reloaded the page (for example)
+       And the function only called when clicking accept...
+       */}
+      <CookieNotice/>
+    </div>
+  );
 }
 
 export default CookieInjector;
