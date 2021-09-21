fae8a73502e6670568f2f1b5627c6b767516322c
Raz Luvaton <rluvaton@gmail.com>
2021-09-22 00:45:09 +0300

    fix(coming-soon): make navbar menu item not break and animate its position
    the text broke on smaller screens such as iPhone 12
    
    working on #7

--- src/pages/shots/ComingSoon/components/NavBar/index.tsx
+++ src/pages/shots/ComingSoon/components/NavBar/index.tsx
@@ -4,8 +4,8 @@ import styles from './index.module.scss';
 
 const NavBar: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <span className={styles.item}>How soon</span>
+    <div className={styles.menu}>
+      <span className={styles.item} data-selected="true">How soon</span>
 
       <span className={styles.item}>About</span>
       <span className={styles.item}>Features</span>
