fdb6cb1a98940dbbb497f49d846908b8aabcc4d7
Raz Luvaton <rluvaton@gmail.com>
2021-09-22 18:13:12 +0300

    refactor(coming-soon): added title for each circle in the page indicator
    working on #7

--- src/pages/shots/ComingSoon/components/PageIndicator/index.tsx
+++ src/pages/shots/ComingSoon/components/PageIndicator/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styles from './index.module.scss';
 
-const Circle: React.FC<{ selected?: boolean }> = ({ selected }) => (
-  <div className={styles.circle} data-selected={selected}/>
+const Circle: React.FC<{ selected?: boolean, title: string }> = ({ title, selected }) => (
+  <div role="img" title={title} className={styles.circle} data-selected={selected}/>
 );
 
 const PageIndicator: React.FC = () => (
-  <div className={styles.pageIndicator}>
-    <Circle selected/>
-    <Circle/>
-    <Circle/>
-    <Circle/>
+  <div role="menu" className={styles.pageIndicator}>
+    <Circle title="How soon" selected/>
+    <Circle title="About"/>
+    <Circle title="Features"/>
+    <Circle title="Contacts"/>
   </div>
 );
 
