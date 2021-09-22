9ce89c7e6ef4e2a20a4ecf539f0bb4e627ca49a6
Raz Luvaton <rluvaton@gmail.com>
2021-09-22 17:20:00 +0300

    feat(coming-soon): add aria role for the navbar
    working on #7

--- src/pages/shots/ComingSoon/components/NavBar/index.tsx
+++ src/pages/shots/ComingSoon/components/NavBar/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import styles from './index.module.scss';
 
 
-const NavBar: React.FC = () => {
-  return (
-    <div className={styles.menu}>
-      <span className={styles.item} data-selected="true">How soon</span>
+const NavBar: React.FC = () => (
+  <div role="menu" className={styles.menu}>
+    <span role="menuitem" className={styles.item} data-selected="true">How soon</span>
 
-      <span className={styles.item}>About</span>
-      <span className={styles.item}>Features</span>
-      <span className={styles.item}>Contacts</span>
-    </div>
-  );
-}
+    <span role="menuitem" className={styles.item}>About</span>
+    <span role="menuitem" className={styles.item}>Features</span>
+    <span role="menuitem" className={styles.item}>Contacts</span>
+  </div>
+);
 
 export default NavBar;
