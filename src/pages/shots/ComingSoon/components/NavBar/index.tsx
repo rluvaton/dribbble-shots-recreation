44c886f2443a273ee9e68595b34e11aa56a03e83
Raz Luvaton <rluvaton@gmail.com>
2021-09-22 17:28:10 +0300

    refactor(coming-soon): use nav instead of div in navbar
    working on #7

--- src/pages/shots/ComingSoon/components/NavBar/index.tsx
+++ src/pages/shots/ComingSoon/components/NavBar/index.tsx
@@ -3,13 +3,13 @@ import styles from './index.module.scss';
 
 
 const NavBar: React.FC = () => (
-  <div role="menu" className={styles.menu}>
-    <span role="menuitem" className={styles.item} data-selected="true">How soon</span>
+  <nav className={styles.menu}>
+    <span data-selected="true">How soon</span>
 
-    <span role="menuitem" className={styles.item}>About</span>
-    <span role="menuitem" className={styles.item}>Features</span>
-    <span role="menuitem" className={styles.item}>Contacts</span>
-  </div>
+    <span>About</span>
+    <span>Features</span>
+    <span>Contacts</span>
+  </nav>
 );
 
 export default NavBar;
